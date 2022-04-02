232f99c632fba3eb80b21b1c78b1044786291cf9
Doan LN <developer.vcc@gmail.com>
2022-04-03 00:03:45 +0700

    2222

--- src/dev/templates/repository.php
+++ src/dev/templates/repository.php
@@ -3,27 +3,33 @@
 namespace App\Repositories\FOLDER;
 
 use Gomee\Repositories\BaseRepository;
-
+/**
+ * validator 
+ * 
+ */
+use App\Validators\FOLDER\NAMEValidator;
+use App\Masks\FOLDER\MODELMask;
+use App\Masks\FOLDER\MODELCollection;
 class NAMERepository extends BaseRepository
 {
     /**
      * class chứ các phương thức để validate dử liệu
      * @var string $validatorClass 
      */
-    protected $validatorClass = 'FOLDER\NAMEValidator';
+    protected $validatorClass = NAMEValidator::class;
     /**
      * tên class mặt nạ. Thường có tiền tố [tên thư mục] + \ vá hậu tố Mask
      *
      * @var string
      */
-    protected $maskClass = 'FOLDER\MODELMask';
+    protected $maskClass = MODELMask::class;
 
     /**
      * tên collection mặt nạ
      *
      * @var string
      */
-    protected $maskCollectionClass = 'FOLDER\MODELCollection';
+    protected $maskCollectionClass = MODELCollection::class;
 
     /**
      * @var \App\Models\MODEL
