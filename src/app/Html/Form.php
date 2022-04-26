5bb4cc512a9edace3254cf72c46483534cc5df68
Doan LN <developer.vcc@gmail.com>
2022-04-26 16:31:50 +0700

    update feature

--- src/app/Html/Form.php
+++ src/app/Html/Form.php
@@ -32,7 +32,7 @@ class Form extends HtmlDom implements Countable, ArrayAccess, IteratorAggregate,
     protected $errors = null;
 
     /**
-     * @var array du lieu form dang nang
+     * @var Arr du lieu form dang nang
      */
 
     protected $form_data = null;
@@ -559,7 +559,10 @@ class Form extends HtmlDom implements Countable, ArrayAccess, IteratorAggregate,
         return count($this->_data);
     }
 
-
+    public function formData($key = null){
+        if($key != null) return $this->form_data->get($key);
+        return $this->form_data->all();
+    }
 
 
     public function toArray()
