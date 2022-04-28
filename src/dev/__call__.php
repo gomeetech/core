eda522d44188fd34ed77ec1ba63176ba514fe1fd
Doan LN <developer.vcc@gmail.com>
2022-04-28 17:30:58 +0700

    update feature

--- src/dev/__call__.php
+++ src/dev/__call__.php
@@ -24,6 +24,8 @@ function __call__($args = [])
             switch ($f) {
                 case "make":
                 case 'create':
+                case 'm':
+                case 'c':
                     if (isset($param[0])) {
                         $t = array_shift($param);
                         $p = get_args_params($param);
@@ -32,6 +34,39 @@ function __call__($args = [])
                             call_user_func_array("create_{$t}", $args);
                         } elseif (in_array(strtolower($t), ['service', 'provider', 'serviceproviders'])) {
                             call_user_func_array("create_provider", $args);
+                        } else {
+                            $t = strtolower($t);
+                            switch ($t) {
+                                case 'r':
+                                case 'repository':
+                                    call_user_func_array("make_repository", $args);
+                                    break;
+                                case 'm':
+                                case 'model':
+                                    call_user_func_array("make_model", $args);
+                                    break;
+                                case 'mk':
+                                case 'mask':
+                                    call_user_func_array("make_mask", $args);
+                                    break;
+                                case 'c':
+                                case 'ctrl':
+                                case 'controller':
+                                    call_user_func_array("make_controller", $args);
+                                    break;
+
+                                case 'j':
+                                case 'jm':
+                                case 'jsonmodule':
+                                case 'jmodule':
+                                    call_user_func_array("make_json_module", $args);
+                                    break;
+
+
+                                default:
+                                    echo "what you want to make? \n\t - c, controller\n\t - m, -model\n\t - mk, mask\n\t - r, repository\n\t - cmd, command";
+                                    break;
+                            }
                         }
                     }
                     break;
@@ -43,7 +78,7 @@ function __call__($args = [])
                         $args = array_merge([$p['params']], $p['args']);
                         if ($t == 'table') {
                             call_user_func_array("alter_{$t}", $args);
-                        } 
+                        }
                     }
                     break;
             }
