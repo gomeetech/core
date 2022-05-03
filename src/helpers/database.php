780bf1fd279a2fdbc706704e9006d50b47671792
Lê Ngọc Doãn <doanln16@gmail.com>
2022-05-03 14:31:37 +0700

    update feature

--- src/helpers/database.php
+++ src/helpers/database.php
@@ -5,9 +5,10 @@ use Illuminate\Support\Facades\Config;
 
 if(!function_exists('check_db_migrate')){
     function check_db_migrate(){
-        return app(MyAdmin::class, [Config::get("")]);
+        // return app(MyAdmin::class, [Config::get("")]);
         $myConfig = Config::get('database.myadmin', []);
-        $this->myAdmin = new MyAdmin($myConfig['host'], $myConfig['username'], $myConfig['password']);
-        $this->myAdmin->connect();
+        $myAdmin = new MyAdmin($myConfig['host'], $myConfig['username'], $myConfig['password']);
+        $myAdmin->connect();
+        return $myAdmin->tableExists(Config::get('database.connections.mysql.database'), 'users');
     }
 }
